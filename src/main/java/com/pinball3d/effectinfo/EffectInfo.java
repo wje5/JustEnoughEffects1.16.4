7a1ce86664f3f52d03921b4eecb2a16cfbb7cce4
LiskoSlayer63 <markus.ahola@hotmail.com>
2021-02-24 01:08:12 +0200

    Fixed compatibility error with servers

--- src/main/java/com/pinball3d/effectinfo/EffectInfo.java
+++ src/main/java/com/pinball3d/effectinfo/EffectInfo.java
@@ -1,8 +1,18 @@
 package com.pinball3d.effectinfo;
 
+import org.apache.commons.lang3.tuple.Pair;
+
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(EffectInfo.MODID)
 public class EffectInfo {
 	public static final String MODID = "effectinfo";
+	
+	public EffectInfo()
+	{
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+	}
 }
